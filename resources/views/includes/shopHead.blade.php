5cb39cbf2bb6c139b6c6a2d116772ae367320592
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-04-20 17:21:15 +0300

    Update includes/shopHeader.blade.php - remove preloader html Update layouts/shop.blade.php - add shopHead in code Create includes/shopHead.blade.php

---
+++ resources/views/includes/shopHead.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+
--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    @include('includes.head')
+    @include('includes.shopHead')
 </head>
 <body>
     @include('includes.shopHeader')
