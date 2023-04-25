a90ee051cd9dc620138e71d6216aa1d9d83c3acc
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-04-25 12:41:30 +0300

    Update Shop/ShopController.php - implement new logic for search

--- app/Http/Controllers/Shop/ShopController.php
+++ app/Http/Controllers/Shop/ShopController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Elastic\Elasticsearch\ClientBuilder;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpClient\CurlHttpClient;
 
 class ShopController extends Controller
 {
+    public function __construct(private CurlHttpClient $client)
+    {
+    }
+
     /**
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
@@ -20,15 +25,20 @@ class ShopController extends Controller
             $products = Product::paginate(5);
 
             if ($request->getMethod() == 'POST' && !empty($request->get('product'))) {
-                $client = ClientBuilder::create()->setHosts(['localhost:9200'])->build();
-                $response = $client->search([
-                    'index' => 'products',
-                    'query' => [
-                        'match' => [
-                            'name' => $request->get('product')
+                $searchResponseJson = $this->client->request(
+                    "GET",
+                    config('elasticsearch.url') . "/products",
+                    [
+                        "headers" => [
+                            "Content-Type" => "application/json"
+                        ],
+                        "query" => [
+                            "match" => [
+                                "name" => $request->get('product')
+                            ]
                         ]
                     ]
-                ]);
+                );
             }
         } catch (\Exception $exception) {
             Log::error($exception->getMessage());
